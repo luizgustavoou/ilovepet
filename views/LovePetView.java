54a679740ca8743a035fc0538db6aa1d9c36dc13
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-10-08 16:08:41 -0300

    criar arquivo view (so para criar, pois nao sera usado)

---
+++ views/LovePetView.java
@@ -0,0 +1,6 @@
+package ilovepet.views;
+
+// A prática não pede para criar uma interface gráfica, portanto nao precisa. Seria chamando os metodos do controller pela classe Main
+public class LovePetView {
+    
+}
