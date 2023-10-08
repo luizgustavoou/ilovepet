aae3e08902ce6b517c649035cfc98d89636410db
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-10-08 14:44:49 -0300

    criar animais

---
+++ models/Animal.java
@@ -0,0 +1,34 @@
+package ilovepet.models;
+
+public abstract class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    abstract void walk();
+
+    abstract void eat();
+
+
+
+}
---
+++ models/Rabbit.java
@@ -0,0 +1,22 @@
+package ilovepet.models;
+
+public class Rabbit extends Animal{
+
+    public Rabbit(String name, int age) {
+        super(name, age);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    void walk() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'walk'");
+    }
+
+    @Override
+    void eat() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eat'");
+    }
+    
+}
